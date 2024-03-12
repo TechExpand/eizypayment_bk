da55e3412e2a877aa9a421980725544bb73819d0
TechExpand <dailydevo9@gmail.com>
2024-03-12 16:56:04 +0100

    done

--- controllers/invoice.ts
+++ controllers/invoice.ts
@@ -6,6 +6,8 @@ import { UserState, UserStatus, Users } from "../models/Users";
 import { compare, hash } from "bcryptjs"
 import config from '../config/configSetup';
 import network from "../config/network.json";
+import mainnet from "../config/paymentMainNet.json";
+import testnet from "../config/paymentTestNet.json";
 import { sign } from "jsonwebtoken";
 import { compareTwoStrings } from 'string-similarity';
 
@@ -14,6 +16,7 @@ const util = require('util')
 import { Invoice } from "../models/Invoice";
 import { Tokens } from "../models/Token";
 import { UserTokens } from "../models/UserToken";
+import { parseJsonText } from "typescript";
 const fs = require("fs");
 const axios = require('axios')
 
@@ -22,8 +25,23 @@ const axios = require('axios')
 
 export const createInvoice = async (req: Request, res: Response) => {
   const { id } = req.user;
-  const { lineItems, overdueAt, network, customerId } = req.body;
+  const { lineItems, overdueAt, network, customerId, token } = req.body;
   const user = await Users.findOne({ where: { id } })
+  console.log(util.inspect({
+    customerIds: [customerId],
+    products: [],
+    lineItems: lineItems,
+    overdueAt: new Date(overdueAt).toISOString(),
+    inputData: [{
+      "key": "name",
+      "value": user?.email
+    }],
+    memo: null,
+    gateway: {
+      managed: { methods: [{ network, token, discountPercentOff: null }] }
+    }
+  }, false, null, true /* enable colors */))
+
   try {
     const response = await axios({
       method: 'POST',
@@ -31,31 +49,7 @@ export const createInvoice = async (req: Request, res: Response) => {
       headers: { 'Content-Type': 'application/json', Authorization: `${config.RADON}` },
       data: {
         customerIds: [customerId],
-        products: [
-          // {
-          //   product: {
-          //     id: '497f6eca-6276-4993-bfeb-53cbbbba6f08',
-          //     name: 'string',
-          //     description: 'string',
-          //     organizationId: '7bc05553-4b68-44e8-b7bc-37be63c6d9e9',
-          //     addOns: [{ id: '497f6eca-6276-4993-bfeb-53cbbbba6f08', name: 'string', price: 0 }],
-          //     chargingIntervalSeconds: 0,
-          //     currency: 'USD',
-          //     price: 0,
-          //     imageUrl: 'string',
-          //     createdAt: '2019-08-24T14:15:22Z',
-          //     updatedAt: '2019-08-24T14:15:22Z',
-          //     isInventoried: true,
-          //     quantity: 0,
-          //     isArchived: true,
-          //     meteredBudget: 0,
-          //     meteredChargingInterval: 0,
-          //     allowanceDuration: 0,
-          //     sendSubscriptionEmails: true
-          //   },
-          //   quantity: 0
-          // }
-        ],
+        products: [],
         lineItems: lineItems,
         overdueAt: new Date(overdueAt).toISOString(),
         inputData: [{
@@ -64,10 +58,12 @@ export const createInvoice = async (req: Request, res: Response) => {
         }],
         memo: null,
         gateway: {
-          managed: { methods: [{ network, token: null, discountPercentOff: null }] }
+          managed: { methods: [{ network, token, discountPercentOff: null }] }
         }
       }
     })
+
+    console.log(util.inspect(response.data, false, null, true /* enable colors */))
     const invoice = await Invoice.create({
       randoId: response.data[0].id,
       organizationId: response.data[0].organizationId,
@@ -123,22 +119,23 @@ export const fetchSignleInvoice = async (req: Request, res: Response) => {
       url: `https://api.radom.network/invoice/${id}`,
       headers: { 'Content-Type': 'application/json', Authorization: `${config.RADON}` },
     })
+    const data = JSON.parse(JSON.stringify(response.data))
     await invoice!.update({
-      organizationId: response.data.organizationId,
-      seller: response.data.seller,
-      customer: response.data.customer,
-      gateway: response.data.gateway,
-      products: response.data.products,
-      lineItems: response.data.lineItems,
-      issuedAt: response.data.issuedAt,
-      paidAt: response.data.paidAt,
-      voidedAt: response.data.voidedAt,
-      overdueAt: response.data.overdueAt,
-      inputData: response.data.inputData,
-      status: response.data.status,
-      memo: response.data.memo,
-      url: response.data.url,
-      payment: response.data.payment,
+      organizationId: data.organizationId,
+      seller: data.seller,
+      customer: data.customer,
+      gateway: data.gateway,
+      products: data.products,
+      lineItems: data.lineItems,
+      issuedAt: data.issuedAt,
+      paidAt: data.paidAt,
+      voidedAt: data.voidedAt,
+      overdueAt: data.overdueAt,
+      inputData: data.inputData,
+      status: data.status,
+      memo: data.memo,
+      url: data.url,
+      payment: data.payment,
     })
     const newInvoice = await Invoice.findOne({ where: { randoId: id } })
     return successResponse(res, "Successful", newInvoice);
@@ -156,7 +153,8 @@ export const fetchSignleInvoice = async (req: Request, res: Response) => {
 
 
 export const fetchAllNetwork = async (req: Request, res: Response) => {
-  return successResponse(res, "Successful", network);
+  const { type } = req.query
+  return successResponse(res, "Successful", type == "TESTNET" ? testnet : mainnet);
 
 }
 
@@ -182,22 +180,23 @@ export const webhook = async (req: Request, res: Response) => {
         url: `https://api.radom.network/invoice/${body.radomData.invoice.invoiceId}`,
         headers: { 'Content-Type': 'application/json', Authorization: `${config.RADON}` },
       })
+      const data = JSON.parse(JSON.stringify(response.data))
       await invoice!.update({
-        organizationId: response.data.organizationId,
-        seller: response.data.seller,
-        customer: response.data.customer,
-        gateway: response.data.gateway,
-        products: response.data.products,
-        lineItems: response.data.lineItems,
-        issuedAt: response.data.issuedAt,
-        paidAt: response.data.paidAt,
-        voidedAt: response.data.voidedAt,
-        overdueAt: response.data.overdueAt,
-        inputData: response.data.inputData,
-        status: response.data.status,
-        memo: response.data.memo,
-        url: response.data.url,
-        payment: response.data.payment,
+        organizationId: data.organizationId,
+        seller: data.seller,
+        customer: data.customer,
+        gateway: data.gateway,
+        products: data.products,
+        lineItems: data.lineItems,
+        issuedAt: data.issuedAt,
+        paidAt: data.paidAt,
+        voidedAt: data.voidedAt,
+        overdueAt: data.overdueAt,
+        inputData: data.inputData,
+        status: data.status,
+        memo: data.memo,
+        url: data.url,
+        payment: data.payment,
       })
       const newInvoice = await Invoice.findOne({ where: { randoId: body.radomData.invoice.invoiceId } })
       let token = newInvoice?.payment.managed.conversionRates[0].to
