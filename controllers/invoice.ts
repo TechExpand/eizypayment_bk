00ae7cdf3172f27adf1b4540235fbe008e03807c
TechExpand <dailydevo9@gmail.com>
2024-03-12 20:29:59 +0100

    ∂one

--- controllers/invoice.ts
+++ controllers/invoice.ts
@@ -202,8 +202,8 @@ export const webhook = async (req: Request, res: Response) => {
       const newInvoice = await Invoice.findOne({ where: { randoId: body.radomData.invoice.invoiceId } })
       let y = JSON.parse(JSON.stringify(newInvoice?.dataValues.payment))
       let x = JSON.parse(JSON.stringify(newInvoice?.payment))
-      console.log(y.managed)
-      console.log(x.managed)
+      console.log(y)
+      console.log(x)
       let token = newInvoice?.dataValues.payment.managed.conversionRates[0].to
 
       let amountToCredit = body.eventData.managedPayment.amount
